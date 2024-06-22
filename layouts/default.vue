19171813b61ae630fe5e5c86ceae29b5d125143f
Артём <63098004+eleness@users.noreply.github.com>
2024-06-22 19:41:40 +0300

    Added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+// Init Nuxt Object
+const nuxtApp = useNuxtApp();
+
+//'Loading' Reactive Variable
+const loading = ref(true);
+
+// Preloader delay miliseconds
+const preloader_delay = 1000;
+
+// Checking render status
+nuxtApp.hook("page:start", () => {
+    loading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+setTimeout(() => {      
+    loading.value = false;
+}, preloader_delay);
+});
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+/* Preloader transition*/
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.15s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
