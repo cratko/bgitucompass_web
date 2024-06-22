839fc7a0c3c665dfd89b77f426778c59579b236f
Артём <63098004+eleness@users.noreply.github.com>
2024-06-22 19:47:18 +0300

    Created NavBar component

---
+++ components/NavBar.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+  //Init reactive variable
+  const model = defineModel();
+</script>
+
+<template>
+    <v-layout class="overflow-visible" style="height: 56px;">
+      <v-bottom-navigation
+        v-model="model"
+        color="blue"
+        grow
+      >
+        <v-btn 
+        variant="plain" 
+        :disabled="0==model"
+        v-ripple.center>
+          <v-icon>mdi-calendar-month</v-icon>
+  
+          <p class="navbar_title">расписание</p>
+        </v-btn>
+  
+        <v-btn 
+        variant="plain" 
+        :disabled="1==model"
+        v-ripple.center>
+          <v-icon>mdi-account-group-outline</v-icon>
+  
+          <p class="navbar_title">преподаватели</p>
+        </v-btn>
+  
+        <v-btn 
+        variant="plain" 
+        :disabled="2==model"
+        v-ripple.center>
+          <v-icon>mdi-account-circle</v-icon>
+  
+          <p class="navbar_title">профиль</p>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-layout>
+  </template>
+
+<style scoped>
+.navbar_title {
+    font-size: 0.75rem;
+}
+
+.v-btn--disabled {
+  opacity: 1;
+}
+.v-btn--disabled:hover {
+  opacity: 1;
+}
+</style>
